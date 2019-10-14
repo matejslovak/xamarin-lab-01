820e4dd5bde7183197f6c77c8bec038035aa6c97
matejslovak <joggurtik1@gmail.com>
2019-10-14 08:12:08 +0200

    xamarin lab

---
+++ Xamarin-lab-01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Xamarin_lab_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Xamarin Lab");
+        }
+    }
+}
